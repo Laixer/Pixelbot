1e0b94b80f9df52972169a0b3012830506e2f2b7
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-04-17 16:27:11 +0200

    client example

--- World.gd
+++ World.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+const HOST: String = "127.0.0.1"
+const PORT: int = 30051
+
 # Speed at which the player moves, adjust as needed.
 var player_speed = 200
 # Initialize movement vector.
@@ -8,6 +11,9 @@ var player_movement = Vector2.ZERO
 var joystick_start_position = Vector2(950, 450)
 var joystick_postion = Vector2(950, 450)
 var joystick_max_handle_distance = 50
+
+const Client = preload("res://glonax-client.gd")
+var _client: Client = Client.new()
 
 enum Direction {
 	LEFT = 0,
@@ -18,6 +24,27 @@ func _ready():
 	# Ensure the handle is centered at start (neutral position).
 	var joystick_handle = get_node("LargeHandleFilled")
 	joystick_handle.position = joystick_start_position
+	
+	_client.connected.connect(_handle_client_connected)
+	_client.disconnected.connect(_handle_client_disconnected)
+	_client.error.connect(_handle_client_error)
+	_client.message.connect(_handle_client_message)
+	
+	add_child(_client)
+	
+	_client.connect_to_host(HOST, PORT)
+	
+func _handle_client_connected() -> void:
+	print("Client is connected.")
+
+func _handle_client_disconnected() -> void:
+	print("Client disconnected from server.")
+
+func _handle_client_error() -> void:
+	print("Client error.")
+	
+func _handle_client_message(message_type: Client.MessageType, data: PackedByteArray) -> void:
+	print("We got message: " + str(message_type) + " with data: " + str(data))
 	
 func _process(delta):
 	var player = get_node("Player") # Adjust the path if necessary.
@@ -35,7 +62,7 @@ func _input(event):
 			if event.axis_value == 1:
 				max_boom(Direction.RIGHT)
 			elif event.axis_value == -1:
-				max_boom(Direction.LEFT)				
+				max_boom(Direction.LEFT)
 		elif event.axis == 1: # Y axis
 			player_movement.y = event.axis_value
 			joystick_postion.y = event.axis_value * joystick_max_handle_distance
@@ -44,6 +71,18 @@ func max_boom(direction: Direction):
 	match direction:
 		Direction.LEFT:
 			print("Going left")
+			var motion = Client.MotionMessage.new()
+			motion.command = Client.CHANGE
+			
+			var change_set = Client.MotionChangeSetMessage.new()
+			change_set.actuator = 2
+			change_set.value = 32_000
+			
+			motion.value_list = [change_set]
+			
+			print("Motion bytes: ", motion.to_bytes())
+			_client.send(Client.MessageType.MOTION, motion.to_bytes())
+			
 		Direction.RIGHT:
 			print("Going right")
 		_:
