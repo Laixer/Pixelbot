704b412a48bcd345964dd3ce0c47a547f8346d54
Daan van Oppenraaij <daan@laixer.com>
2024-04-17 15:50:39 +0200

    Add max_boom function for testing

--- World.gd
+++ World.gd
@@ -9,6 +9,11 @@ var joystick_start_position = Vector2(950, 450)
 var joystick_postion = Vector2(950, 450)
 var joystick_max_handle_distance = 50
 
+enum Direction {
+	LEFT = 0,
+	RIGHT = 1
+}
+
 func _ready():
 	# Ensure the handle is centered at start (neutral position).
 	var joystick_handle = get_node("LargeHandleFilled")
@@ -27,6 +32,19 @@ func _input(event):
 		if event.axis == 0: # X axis
 			player_movement.x = event.axis_value
 			joystick_postion.x = event.axis_value * joystick_max_handle_distance
+			if event.axis_value == 1:
+				max_boom(Direction.RIGHT)
+			elif event.axis_value == -1:
+				max_boom(Direction.LEFT)				
 		elif event.axis == 1: # Y axis
 			player_movement.y = event.axis_value
 			joystick_postion.y = event.axis_value * joystick_max_handle_distance
+
+func max_boom(direction: Direction):
+	match direction:
+		Direction.LEFT:
+			print("Going left")
+		Direction.RIGHT:
+			print("Going right")
+		_:
+			print("Unknown direction")
