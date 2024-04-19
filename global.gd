56a97bf110d697d796aa13bd106fae4bc7f2eea2
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-04-19 14:08:49 +0200

    Refactor connection handling and add client connection status

--- global.gd
+++ global.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var hostname: String
+var host: String
+var port: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- main.gd
+++ main.gd
@@ -1,7 +1,42 @@
 extends Node2D
 
+const Client = preload("res://glonax-client.gd")
+var _client: Client = Client.new("godot/4.2")
 
+func _ready():
+	_client.connected.connect(_handle_client_connected)
+	_client.error.connect(_handle_client_error)
 
-func _on_play_pressed():
-	Global.hostname = $HostnameInput.text
-	get_tree().change_scene_to_file("res://world.tscn")
+	add_child(_client)
+
+func _handle_client_connected() -> void:
+	$ConnectionStatus.text = "Connected to " + Global.host + ":" + str(Global.port)
+
+func _handle_client_error() -> void:
+	$ConnectionStatus.text = "Failed to connect to " + Global.host + ":" + str(Global.port)
+
+func parse_hostname(hostname: String):
+	var parts = hostname.split(":")
+	if parts.size() == 1:
+		return { "host": hostname, "port": 30051} 
+	elif parts.size() == 2:
+		var host = parts[0]
+		var port = parts[1].to_int()
+		return { "host": host, "port": port} 
+	else:
+		return null
+
+func _on_connect_pressed():
+	var hostname = $HostnameInput.text
+	$HostnameInput.editable = false
+
+	var result = parse_hostname(hostname)
+	if result:
+		$ConnectionStatus.text = "Connecting to... " + result.host + ":" + str(result.port)
+		Global.host = result.host
+		Global.port = result.port
+		_client.connect_to_host(result.host, result.port)
+		#get_tree().change_scene_to_file("res://world.tscn")
+	else:
+		$ConnectionStatus.text = "Invalid hostname and port"
+		$HostnameInput.editable = true
