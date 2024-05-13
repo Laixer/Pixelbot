76d1eba08ab7b256e9d48fda752a4bdcdd1fb653
Daan van Oppenraaij <daan@laixer.com>
2024-05-13 17:34:21 +0200

    remove comment

--- JoystickInner.gd
+++ JoystickInner.gd
@@ -3,4 +3,4 @@ extends Sprite2D
 var start_position = Vector2()  # Initialize it with a default value
 
 func _ready():
-	start_position = position  # Assuming you want to store the initial position
+	start_position = position  
